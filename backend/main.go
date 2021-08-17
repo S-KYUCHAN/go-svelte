5c6db34655232f4fd75a02d6046aab75abe22255
S-KYUCHAN <skc01012001@naver.com>
2021-08-17 06:13:10 -0700

    modified CORS and css

--- backend/main.go
+++ backend/main.go
@@ -91,10 +91,9 @@ func welcomePage(w http.ResponseWriter, h *http.Request) {
 
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// w.Header().Set("Access-Control-Allow-Origin:", "*")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers:", "*")
-		w.Header().Set("Access-Control-Allow-Methods:", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
 		w.Header().Set("Content-Type", "application/json")
 		
 		if r.Method == "OPTIONS" {
--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,18 +1,35 @@
 <script>
 	// export let name;
 	import axios from 'axios'
+	import { onMount } from 'svelte';
 
+	$: articles = [];
 
-	$: data = [];
-	$: items = axios.get(`http://localhost:8000/articles/`).then(response => data = response.data)
-	console.log(data)
+	async function getAPI() {
+		return await axios.get(`http://localhost:8000/articles/`)
+			.then(response => response.data)
+	}
+
+	onMount(async () => {
+		const res = await getAPI();
+		articles = res;
+		console.log(articles);
+	});
+
 </script>
 
 <main>
-	<!-- <h1>Hello {name}!</h1> -->
 	<h1>Hello kyuchan!</h1>
-	<p>Let's start SVELTE!</p>
-	<p>{data}</p>
+	<h2>Let's start SVELTE!</h2>
+	<ul class="articleList">
+		{#each articles as article}
+			<div class="box">
+				<div>{article.title}</div>
+				<div>{article.content}</div>	
+			</div>
+		{/each}
+	</ul>
+	<!-- <p>{data}</p> -->
 </main>
 
 <style>
@@ -30,10 +47,21 @@
 		font-weight: 100;
 	}
 
-	p {
+	h2 {
 		color: orange;
 		font-size: 2rem;
 		font-weight: 100;
+	}
+
+	.box {
+		/* position: relative; */
+		display: inline-block;
+		width: 50%;
+		border: 1px solid #aaa;
+		border-radius: 2px;
+		box-shadow: 2px 2px 8px rgba(0,0,0,0.1);
+		padding: 1em;
+		margin: 2rem 0 2em 0;
 	}
 
 	@media (min-width: 640px) {
